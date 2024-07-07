d5120f8479b81c593b3daf38a5260423cf004598
Dinesh kumar <reachtodinesh@gmail.com>
2024-07-07 13:01:48 +0530

    removed unwanted code

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <p className="read-the-docs">50days50projects attempt in react</p>
+      <a href="https://github.com/bradtraversy/50projects50days/tree/master?tab=readme-ov-file">
+        HTML only - original project link
+      </a>
+      <br />
+      <a href="">React based - Link needs to be added</a>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
