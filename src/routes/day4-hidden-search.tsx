cf2de789fbef5348a427cee7bd5555f2f8ebd4d0
Dinesh kumar <reachtodinesh@gmail.com>
2024-07-09 05:33:01 +0530

    day 4 completed

--- src/routes/day4-hidden-search.tsx
+++ src/routes/day4-hidden-search.tsx
@@ -1,10 +1,29 @@
-import "../css/day4.module.css";
+import { useState } from "react";
+import styles from "../css/day4.module.css";
+import { Search } from "lucide-react";
 
 export default function Day4HiddenSearch() {
+  const [search, setSearch] = useState(false);
+
+  const handleclick = () => {
+    setSearch(!search);
+  };
+
   return (
     <div>
       <a href="/">Back to Home</a>
       <h1>Day 4: Hidden search</h1>
+      <div className={`${styles.search} ${search ? styles.active : ""}`}>
+        <input type="text" placeholder="search.." />
+        <button
+          title="search"
+          type="button"
+          className={styles.btn}
+          onClick={handleclick}
+        >
+          <Search />
+        </button>
+      </div>
     </div>
   );
 }
